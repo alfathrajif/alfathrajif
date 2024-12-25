6a605cc30e7d47e3fe24f76350456d384182423f
Alfath Rajif <alfath.code@gmail.com>
2024-12-25 19:59:31 +0700

    Refactor image file names in assets

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,4 +1,4 @@
-import projectImage1 from "./We-Build-Your-Construction-With-Trust-JoDesign-Construction-10-31-2024_09_14_PM.png";
+import projectImage1 from "./JoDesign-Construction-10-31-2024_09_14_PM.png";
 import projectImage2 from "./Alfath-Rajif-Web-Programmer-Developer-10-31-2024_09_50_PM.png";
 
 export { projectImage1, projectImage2 };
